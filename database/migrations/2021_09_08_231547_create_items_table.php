5eb88fa8152a866556ae725cfa1d777552d59e96
jpgomezt <jpgomezxt@gmail.com>
2021-09-08 21:31:56 -0500

    Create migration for item

---
+++ database/migrations/2021_09_08_231547_create_items_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_rented');
+            $table->date('return_date');
+            $table->integer('quantity');
+            $table->integer('subtotal');
+            $table->foreignId('movie_id')->constrained()->onDelete('cascade');
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
