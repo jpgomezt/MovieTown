34a7d67a99c843cc04f28546c77badf62e312e7f
Juan Pablo Gómez <jpgomezxt@gmail.com>
2021-09-23 02:37:35 -0500

    Created Image Services

---
+++ app/Util/ImageLocalStorage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Util;
+
+use App\Interfaces\ImageStorage;
+use Illuminate\Support\Facades\Storage;
+
+class ImageLocalStorage implements ImageStorage
+{
+    public function store($movie_id, $request)
+    {
+        if ($request->hasFile('movie_image')) {
+            Storage::disk('public')->put($movie_id . ".png", file_get_contents($request->file('movie_image')->getRealPath()));
+        }
+    }
+}
