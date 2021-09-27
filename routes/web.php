a4ccec7ff3c55603c9673233aed376d2c229f32b
Juan Pablo Gómez <jpgomezxt@gmail.com>
2021-09-27 18:03:12 -0500

    Deleted User Controller and routes

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,6 @@ Auth::routes();
 
 Route::get('home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('user/list', [App\Http\Controllers\UserController::class, 'list'])->name('user.list');
-Route::get('user/show/{id}', [App\Http\Controllers\UserController::class, 'show'])->name('user.show');
-Route::get('user/delete/{id}', [App\Http\Controllers\UserController::class, 'delete'])->name('user.delete');
-Route::get('user/save', [App\Http\Controllers\UserController::class, 'save'])->name('user.save');
-Route::get('user/addViewedMovie/{id}', [App\Http\Controllers\UserController::class, 'addViewedMovie'])->name('user.addViewedMovie');
-
 Route::get('watchlist/create', [App\Http\Controllers\WatchlistController::class, 'create'])->name('watchlist.create');
 Route::get('watchlist/list', [App\Http\Controllers\WatchlistController::class, 'list'])->name('watchlist.list');
 Route::get('watchlist/show/{id}', [App\Http\Controllers\WatchlistController::class, 'show'])->name('watchlist.show');
