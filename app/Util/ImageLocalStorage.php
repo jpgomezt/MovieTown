5b4a4af3de67a27d436b7bee8ca9d08a0b993bf2
Juan Pablo Gómez <jpgomezxt@gmail.com>
2021-09-27 17:31:16 -0500

    Fixed Image problem

--- app/Util/ImageLocalStorage.php
+++ app/Util/ImageLocalStorage.php
@@ -4,12 +4,14 @@ namespace App\Util;
 
 use App\Interfaces\ImageStorage;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\URL;
 
 class ImageLocalStorage implements ImageStorage
 {
     public function store($movie_id, $request)
     {
         if ($request->hasFile('movie_image')) {
+            Storage::disk('public')->delete($movie_id . ".png");
             Storage::disk('public')->put($movie_id . ".png", file_get_contents($request->file('movie_image')->getRealPath()));
         }
     }
