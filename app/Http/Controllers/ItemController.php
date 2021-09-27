6cd6d440609a19f3e3d7676d48e80624833e09f3
Juan Pablo Gómez <jpgomezxt@gmail.com>
2021-09-26 23:26:26 -0500

    Fixed Item Controller and view

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,27 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Item;
-use App\Models\Movie;
 use App\Models\Order;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ItemController extends Controller
 {
-    public function create(Request $request)
+    public function list($id)
     {
-        $products = $request->session()->get("products");
-        $order = new Order();
-        foreach ($products as $product) {
-            $movie = Movie::find($product['movie_id']);
-            $item = new Item();
-            $item->setOrderId($order->getId());
-            $item->setMovieId($movie->getId());
-            $item->setIsRented($product['is_rented']);
-            $item->setReturnDate($product['return_date']);
-            $item->setQuantity($product['quantity']);
-            $item->setSubtotal($product['quantity'] * $movie->getPrice());
-            $item->save();
+        if (Auth::check()) {
+            $data = [];
+            $data["title"] = "List of items";
+            $data["order"] = Order::with('items.movie')->find($id);
+            return view('item.list', ['data' => $data]);
         }
+        return back();
     }
 }
