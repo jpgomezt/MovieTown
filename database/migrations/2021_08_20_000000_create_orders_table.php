5de0b9f473f7cd9e895161f1aaa2de4b4a9ca813
urregozw <urregozw@gmail.com>
2021-09-28 11:50:04 -0500

    Comentarios de reconocimiento

--- database/migrations/2021_08_20_000000_create_orders_table.php
+++ database/migrations/2021_08_20_000000_create_orders_table.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @author Sebastián Urrego García
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
