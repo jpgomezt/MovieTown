fe893052113b19268e1286cd762d8042b093931c
Juan Pablo Gómez <jpgomezxt@gmail.com>
2021-09-27 20:19:31 -0500

    English messages for cart actions

---
+++ resources/lang/en/message.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'cart_item_exist' => 'Movie is already added to cart',
+    'cart_item_quantity_error' => 'Selected a nonexistent quantity',
+    'cart_item_added' => 'Movie added to cart',
+];
