2ecf035aa0766b434e4fa62b9293500ca640d290
Santiago <santiago.alzate1@hotmail.com>
2021-09-27 23:17:19 -0500

    Poner autor en archivos

--- app/Http/Controllers/WatchlistController.php
+++ app/Http/Controllers/WatchlistController.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @author Santigo Alzate
+ */
+
 namespace App\Http\Controllers;
 
 use App\Models\Movie;
--- database/migrations/2021_09_15_000000_alter_users_table.php
+++ database/migrations/2021_09_15_000000_alter_users_table.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * @author Santiago Alzate
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
