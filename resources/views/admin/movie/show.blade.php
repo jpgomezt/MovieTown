619ecc5973e7d6644e86c534419b7c67974a86d9
Santiago <santiago.alzate1@hotmail.com>
2021-09-27 19:41:51 -0500

    Lang for movies in user and admin views

--- resources/views/admin/movie/show.blade.php
+++ resources/views/admin/movie/show.blade.php
@@ -15,31 +15,32 @@
                     <!-- Movie Content-->
                     <div class="col-md-6 col-sm-12">
                         <!-- Movie Elements-->
-                        <h5 class="card-subtitle">Plot</h5>
+                        <h5 class="card-subtitle">{{ __('movie.plot') }}</h5>
                         <p class="card-text">{{ $data['movie']->getPlot() }}</p>
-                        <h5 class="card-subtitle mt-1">Critics Score</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.critics_score') }}</h5>
                         <div class="card-group card-text divider-custom-line mb-3">
                             @for ($i = 0; $i < round($data['movie']->getCriticsScore()); $i++)
                                 <div class="divider-custom-icon"><i class="fas fa-star text-warning"></i></div>
                             @endfor
                             <p class="card-text pl-2"> {{ $data['movie']->getCriticsScore() }}/5</p>
                         </div>
-                        <h5 class="card-subtitle mt-1">Sell Quantity</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.sell_quantity') }}</h5>
                         <p class="card-text"> {{ $data['movie']->getSellQuantity() }}</p>
-                        <h5 class="card-subtitle mt-1">Sell Price</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.sell_price') }}</h5>
                         <p class="card-text"> {{ $data['movie']->getPrice() }}$</p>
-                        <h5 class="card-subtitle mt-1">Rent Quantity</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.rent_quantity') }}</h5>
                         <p class="card-text"> {{ $data['movie']->getRentQuantity() }}</p>
-                        <h5 class="card-subtitle mt-1">Rent Price</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.rent_price') }}</h5>
                         <p class="card-text"> {{ $data['movie']->getPrice() * 0.2 }}$</p>
                         <!-- Movie Reviews-->
                         <hr>
-                        <h3>Reviews</h3>
+                        <h3>{{ __('movie.reviews') }}</h3>
                         <ul class="list-group">
                             @foreach ($data['movie']->reviews as $review)
                                 @if ($review->getIsVisible())
                                     <li class="list-group-item pb-3 pt-3">
-                                        <h5 class="card-title">Review by {{ $review->user->getName() }}</h5>
+                                        <h5 class="card-title">{{ __('movie.review_by') }}
+                                            {{ $review->user->getName() }}</h5>
                                         <h6 class="card-subtitle mb-2 text-muted"> {{ $review->getDate() }} </h6>
                                         <div class="card-group card-text divider-custom-line">
                                             @for ($i = 0; $i < round($review->getStars()); $i++)
@@ -59,11 +60,12 @@
                             <hr>
                             <ul class="list-group list-group-horizontal justify-content-center">
                                 <li class="list-group-item ">
-                                    <label class="h6">Create Review</label>
+                                    <label class="h6">{{ __('movie.create_review') }}</label>
                                     <form method="POST"
                                         action="{{ route('admin.review.create', ['id' => $data['movie']->getId()]) }}">
                                         @csrf
-                                        <input class="btn btn-success mt-2" type="submit" value="Create">
+                                        <input class="btn btn-success mt-2" type="submit"
+                                            value="{{ __('movie.review_button') }}">
                                     </form>
                                 </li>
                             </ul>
--- resources/views/movie/list.blade.php
+++ resources/views/movie/list.blade.php
@@ -6,41 +6,44 @@
     <div class="container mt-4 mb-4">
         <div class="card">
             <div class="card-header text-center h1">
-                <h1>Available Movies</h1>
+                <h1>{{ __('movie.available_movies') }}</h1>
             </div>
             <div class="card-body">
                 <form method="POST" action="{{ route('movie.filter') }}" enctype="multipart/form-data">
                     @csrf
-                    <h5>Filters</h5>
+                    <h5>{{ __('movie.filters') }}</h5>
                     <div class="row mb-3">
                         <div class="col m-auto">
                             <div class="d-flex">
-                                <input class="form-control flex-fill" type="text" placeholder="Enter title" name="title">
+                                <input class="form-control flex-fill" type="text"
+                                    placeholder="{{ __('movie.title_filter') }}" name="title">
                             </div>
                         </div>
                         <div class="col m-auto">
                             <div class="d-flex flex-column">
-                                <label class="flex-fill text-center" for="criticsScoreRange">Critics Score</label>
+                                <label class="flex-fill text-center"
+                                    for="criticsScoreRange">{{ __('movie.critics_score') }}</label>
                                 <input type="range" class="custom-range flex-fill" name="score" min="0" max="5" step="0.1"
                                     id="criticsScoreRange">
                                 <span class="flex-fill text-center" id="score_value"></span>
                             </div>
                         </div>
                         <div class="col m-auto">
-                            <input class="form-control flex-fill" type="text" placeholder="Enter price" name="price"
-                                value="">
+                            <input class="form-control flex-fill" type="text" placeholder="{{ __('movie.price_filter') }}"
+                                name="price" value="">
                         </div>
                         <div class="col m-auto">
                             <div class="d-flex flex-column custom-control custom-switch">
                                 <input class="flex-fill custom-control-input" type="checkbox" name="rent" id="rentSwitch">
                                 <label class="flex-fill custom-control-label" for="rentSwitch">
-                                    Available for Rent
+                                    {{ __('movie.rent_filter') }}
                                 </label>
                             </div>
                         </div>
                         <div class="col m-auto">
-                            <input class="btn btn-primary ms-4" type="submit" value="Filter">
-                            <a class="btn btn-primary" href=" {{ route('movie.list') }} " role="button">See All</a>
+                            <input class="btn btn-primary ms-4" type="submit" value="{{ __('movie.filter') }}">
+                            <a class="btn btn-primary" href=" {{ route('movie.list') }} "
+                                role="button">{{ __('movie.see_all') }}</a>
                         </div>
                     </div>
                 </form>
@@ -61,7 +64,8 @@
                                         <h6 class="card-subtitle mb-2 text-muted">{{ $movie->getPlot() }}</h6>
                                     @endif
                                 </div>
-                                <a href="{{ route('movie.show', ['id' => $movie->getId()]) }}" class="stretched-link"></a>
+                                <a href="{{ route('movie.show', ['id' => $movie->getId()]) }}"
+                                    class="stretched-link"></a>
                             </div>
                         </div>
                     @endforeach
