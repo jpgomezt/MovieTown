3e64916a6fb7ce183edc6ecfb09ef3e58af816a9
Juan Pablo Gómez <jpgomezxt@gmail.com>
2021-09-28 00:34:25 -0500

    Fixed layout and home. Added lang. Change colors

---
+++ resources/lang/en/home.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'heading_title' => 'Home',
+    'heading_subtitle' => 'An online movie rental and sale page',
+
+    'recommended_title' => 'Recommended Movies',
+    'recommended_error' => 'Go visit some movies so we can recommend you movies in the future',
+    'recommended_movies_link' => 'Movies',
+
+    'about_title' => 'About',
+    'about_content' => 'You can find movies to buy and rent, in addition to creating watchlists and reviews of those movies',
+];
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -5,22 +5,23 @@
     <header class="masthead bg-primary text-white text-center" style="margin-top: -5%">
         <div class="container d-flex align-items-center flex-column">
             <!-- Masthead Heading-->
-            <h1 class="masthead-heading text-uppercase mb-0">Home</h1>
+            <h1 class="masthead-heading text-uppercase mb-0">{{ __('home.heading_title') }}</h1>
             <!-- Icon Divider-->
             <div class="divider-custom divider-light">
                 <div class="divider-custom-line"></div>
-                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-icon"><i class="fas fa-tape"></i></div>
                 <div class="divider-custom-line"></div>
             </div>
             <!-- Masthead Subheading-->
-            <p class="masthead-subheading font-weight-light mb-0">Website - Home</p>
+            <p class="masthead-subheading font-weight-light mb-0">{{ __('home.heading_subtitle') }}</p>
         </div>
     </header>
     <!-- Recommended Movies Section-->
     <section class="page-section portfolio" id="portfolio">
         <div class="container">
             <!-- Recommended Movies Section Heading-->
-            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Recommended Movies</h2>
+            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">
+                {{ __('home.recommended_title') }}</h2>
             <!-- Icon Divider-->
             <div class="divider-custom">
                 <div class="divider-custom-line"></div>
@@ -44,21 +45,21 @@
                 </div>
             @else
                 <div class="col d-flex justify-content-center">
-                    <h5>Go visit some movies so we can recommend you movies in the future</h5>
+                    <h5>{{ __('home.recommended_error') }}</h5>
                 </div>
                 <div class="col d-flex justify-content-center">
                     <a class="badge" href="{{ route('movie.list') }}">
-                        <h1>{{ __('layout.movies_link') }} <i class="fas fa-ticket-alt"></i></h1>
+                        <h1>{{ __('home.recommended_movies_link') }} <i class="fas fa-ticket-alt"></i></h1>
                     </a>
                 </div>
             @endif
         </div>
     </section>
     <!-- About Section-->
-    <section class="page-section bg-primary text-white mb-0" id="about">
+    <section class="page-section bg-primary text-white pb-0 mb-0" id="about">
         <div class="container">
             <!-- About Section Heading-->
-            <h2 class="page-section-heading text-center text-uppercase text-white">About</h2>
+            <h2 class="page-section-heading text-center text-uppercase text-white">{{ __('home.about_title') }}</h2>
             <!-- Icon Divider-->
             <div class="divider-custom divider-light">
                 <div class="divider-custom-line"></div>
@@ -71,9 +72,8 @@
                     <img src="{{ asset('/img/logos/movietown-transparent.png') }}" class="img-fluid rounded"
                         alt="Responsive image">
                 </div>
-                <div class="col-lg-4 mr-auto">
-                    <p class="lead">You can create your own custom avatar for the masthead, change the icon in the
-                        dividers, and add your email address to the contact form to make it fully functional!</p>
+                <div class="col-lg-4 mr-auto d-flex justify-content-center align-self-center">
+                    <p class="lead">{{ __('home.about_content') }}</p>
                 </div>
             </div>
         </div>
