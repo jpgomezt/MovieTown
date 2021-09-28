2a116ac607523b1fae40b14b20eb30ce19e2c996
Juan Pablo Gómez <jpgomezxt@gmail.com>
2021-09-27 20:19:47 -0500

    Added alerts to movie/show

--- resources/views/movie/show.blade.php
+++ resources/views/movie/show.blade.php
@@ -4,6 +4,7 @@
 
 @section('content')
     <div class="container mt-4 mb-4">
+        @include('util.message')
         <div class="card ">
             <div class="card-header text-center h1">{{ $data['movie']->getTitle() }}</div>
             <div class="card-body">
@@ -15,31 +16,32 @@
                     <!-- Movie Content-->
                     <div class="col-md-6 col-sm-12">
                         <!-- Movie Elements-->
-                        <h5 class="card-subtitle">{{ __("movie.plot") }}</h5>
+                        <h5 class="card-subtitle">{{ __('movie.plot') }}</h5>
                         <p class="card-text">{{ $data['movie']->getPlot() }}</p>
-                        <h5 class="card-subtitle mt-1">{{ __("movie.critics_score") }}</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.critics_score') }}</h5>
                         <div class="card-group card-text divider-custom-line mb-3">
                             @for ($i = 0; $i < round($data['movie']->getCriticsScore()); $i++)
                                 <div class="divider-custom-icon"><i class="fas fa-star text-warning"></i></div>
                             @endfor
                             <p class="card-text pl-2"> {{ $data['movie']->getCriticsScore() }}/5</p>
                         </div>
-                        <h5 class="card-subtitle mt-1">{{ __("movie.sell_quantity") }}</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.sell_quantity') }}</h5>
                         <p class="card-text"> {{ $data['movie']->getSellQuantity() }}</p>
-                        <h5 class="card-subtitle mt-1">{{ __("movie.sell_price") }}</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.sell_price') }}</h5>
                         <p class="card-text"> {{ $data['movie']->getPrice() }}$</p>
-                        <h5 class="card-subtitle mt-1">{{ __("movie.rent_quantity") }}</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.rent_quantity') }}</h5>
                         <p class="card-text"> {{ $data['movie']->getRentQuantity() }}</p>
-                        <h5 class="card-subtitle mt-1">{{ __("movie.rent_price") }}</h5>
+                        <h5 class="card-subtitle mt-1">{{ __('movie.rent_price') }}</h5>
                         <p class="card-text"> {{ $data['movie']->getPrice() * 0.2 }}$</p>
                         <!-- Movie Reviews-->
                         <hr>
-                        <h3>{{ __("movie.reviews") }}</h3>
+                        <h3>{{ __('movie.reviews') }}</h3>
                         <ul class="list-group">
                             @foreach ($data['movie']->reviews as $review)
                                 @if ($review->getIsVisible())
                                     <li class="list-group-item pb-3 pt-3">
-                                        <h5 class="card-title">{{ __("movie.review_by") }} {{ $review->user->getName() }}</h5>
+                                        <h5 class="card-title">{{ __('movie.review_by') }}
+                                            {{ $review->user->getName() }}</h5>
                                         <h6 class="card-subtitle mb-2 text-muted"> {{ $review->getDate() }} </h6>
                                         <div class="card-group card-text divider-custom-line">
                                             @for ($i = 0; $i < round($review->getStars()); $i++)
@@ -61,20 +63,21 @@
                                 <li class="list-group-item">
                                     <div class="form-group">
                                         <label class="h6" for="exampleFormControlSelect1">
-                                            {{ __("movie.add_to_cart") }} <i class="fas fa-shopping-cart"></i>
+                                            {{ __('movie.add_to_cart') }} <i class="fas fa-shopping-cart"></i>
                                         </label>
                                         <form method="POST" action="{{ route('cart.add') }}">
                                             @csrf
                                             <div class="custom-control custom-switch">
                                                 <input class="custom-control-input" type="checkbox" name="rent" id="rentSwitch">
                                                 <label class="custom-control-label" for="rentSwitch">
-                                                    {{ __("movie.are_you_renting") }}
+                                                    {{ __('movie.are_you_renting') }}
                                                 </label>
                                             </div>
-                                            <input class="form-control mt-2" type="text" placeholder="{{ __("movie.enter_quantity") }}"
-                                                name="quantity">
+                                            <input class="form-control mt-2" type="number"
+                                                placeholder="{{ __('movie.enter_quantity') }}" name="quantity">
                                             <input type="hidden" name="movie_id" value="{{ $data['movie']->getId() }}">
-                                            <input class="btn btn-warning mt-2" type="submit" value="{{ __("movie.add_to_cart") }}">
+                                            <input class="btn btn-warning mt-2" type="submit"
+                                                value="{{ __('movie.add_to_cart') }}">
                                         </form>
                                     </div>
                                 </li>
@@ -82,7 +85,7 @@
                                     <li class="list-group-item">
                                         <div class="form-group">
                                             <label class="h6" for="exampleFormControlSelect1">
-                                                {{ __("movie.add_to_watchlist") }}
+                                                {{ __('movie.add_to_watchlist') }}
                                             </label>
                                             <form method="POST" action="{{ route('watchlist.addMovie') }}">
                                                 @csrf
@@ -95,17 +98,19 @@
                                                     @endforeach
                                                 </select>
                                                 <input type="hidden" name="movie_id" value="{{ $data['movie']->getId() }}">
-                                                <input class="btn btn-danger mt-2" type="submit" value="{{ __("movie.add_to_watchlist") }}">
+                                                <input class="btn btn-danger mt-2" type="submit"
+                                                    value="{{ __('movie.add_to_watchlist') }}">
                                             </form>
                                         </div>
                                     </li>
                                 @endif
                                 <li class="list-group-item ">
-                                    <label class="h6">{{ __("movie.create_review") }}</label>
+                                    <label class="h6">{{ __('movie.create_review') }}</label>
                                     <form method="POST"
                                         action="{{ route('review.create', ['id' => $data['movie']->getId()]) }}">
                                         @csrf
-                                        <input class="btn btn-success mt-2" type="submit" value="{{ __("movie.review_button") }}">
+                                        <input class="btn btn-success mt-2" type="submit"
+                                            value="{{ __('movie.review_button') }}">
                                     </form>
                                 </li>
                             </ul>
